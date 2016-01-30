4c7ffd09efc2a78b55b3080a1b43c42430b3c092
Jonathan Whiting <jonathan@ignika.com>
2016-01-30 17:31:50 +0000

    Generate table from cards

--- src/ritual.py
+++ src/ritual.py
@@ -13,6 +13,38 @@ post = open(dest_filename, 'w')
 
 s = pre.read()
 s = s.replace('%%%MARKDOWN%%%', content)
+
+card_file = open('src/cards.txt', 'r')
+cards = card_file.read()
+cards = cards.split('\n')
+table = ''
+table += '''
+	<table class="u-full-width">
+		<thead>
+			<tr>
+				<th>Card</th>
+				<th>Description</th>
+			</tr>
+		</thead>
+	'''
+for i in range(len(cards)):
+	card = cards[i]
+	if len(card) < 1 or card[0] == '-':
+		continue
+	table += '''
+		<tbody>
+			<tr>
+				<td>%d</td>
+				<td>%s</td>
+			</tr>
+		</tbody>
+	''' % (i, card)
+table += '''
+	</table>
+'''
+
+s = s.replace('%%%CARD_TABLE%%%', table)
+
 post.write(s)
 post.close()
 pre.close()
